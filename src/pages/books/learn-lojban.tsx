9e30dac4243e44806997f259599593294f7de0c1
lagleki <bangu@lojban.org>
2024-02-22 12:58:18 +0100

    fix: 🐛 add redirect from old learn-lojban

--- src/pages/books/learn-lojban.tsx
+++ src/pages/books/learn-lojban.tsx
@@ -1,12 +1,8 @@
 import { useEffect } from "react";
 
 import { useRouter } from "next/router";
-import { getAllPosts, getPostBySlug } from "../../lib/api";
-import markdownToHtml from "../../lib/markdownToHtml";
-import { Params } from "../[lang]/[...slug]";
 
-const Post = (pam: any) => {
-  console.log({pam});
+const Post = () => {
   const router = useRouter();
   useEffect(() => {
     router.replace("/en" + router.asPath);
