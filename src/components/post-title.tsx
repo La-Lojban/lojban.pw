284e847d670475648cb502cbc4584618f6c165a2
lagleki <gleki.is.my.name@gmail.com>
2024-08-25 21:25:33 +0300

    fix: 🐛 adaptive top bar

--- src/components/post-title.tsx
+++ src/components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const PostTitle = ({ children }: Props) => {
   return (
-    <h1 className="text-6xl md:text-6xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-2 text-center md:text-left">
+    <h1 className="text-6xl md:text-6xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none text-center md:text-left">
       {children}
     </h1>
   )
